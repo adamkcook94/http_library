eff63ef5c67fad0e20564a187a55ccfe3fad836c
adamkcook94 <adamkcook94@hotmail.com>
2022-04-16 00:33:19 +0000

    Completed Lesson 64 notes

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const http = new easyHTTP
+// const http = new easyHTTP;
 
-// Get posts
-http.get('https://jsonplaceholder.typicode.com/posts');
+// Get Posts
+
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts');
+
+// console.log(posts);
+
+// posts is now equal to the data from the API.
+
+// we're also passing through the url parameter as seen in http_library.js with the json typicode API.
+
+// A Callback is a function that is to be executed after another function has finished executing — hence the name ‘call back’.
+
+// We need a callback here because the data is returned before it's been added to 'posts'.
+
+// I believe the first function being carried out is retrieving the data from the API.
+
+// So the reason we'd need a callback is to then execute adding the data to app.js
+
+const http = new easyHTTP;
+
+http.get('https://jsonplaceholder.typicode.com/posts12', function(err, posts) {
+    if(err) { // If null, console.log first parameter of the else in http_library.js, because thats where it goes if there's a status other than 200.
+        console.log(err);
+    } else { // else, log the posts
+        console.log(posts);
+    }
+});
--- http_library.js
+++ http_library.js
@@ -1,23 +1,25 @@
+// Here we are creating a library containing data.
+
+// Constructor function where we are creating the easyHTTP object.
 function easyHTTP() {
-    this.http = new XMLHttpRequest();
+    this.http = new XMLHttpRequest
 }
 
-// Make an HTTP GET Request
-easyHTTP.prototype.get = function(url) {
+// Make HTTP 'GET' request
+
+easyHTTP.prototype.get = function(url, callback) { //
+    // Here we are creating a get prototype within the easyHTTP object.
     this.http.open('GET', url, true);
-    
+
     let self = this;
 
     this.http.onload = function() {
         if(self.http.status === 200) {
-            console.log(self.http.responseText);
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
         }
     }
 
     this.http.send();
 }
-
-// Make an HTTP GET Request
-// Make an HTTP GET Request
-// Make an HTTP GET Request
-// Make an HTTP GET Request
